60be7da8b132786e28f37ff07419ab87a92c2602
שחר בן שמחון <shahar1913@mbp-sl-shr.home>
2026-03-10 22:35:11 -0500

    use firebase env var

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./firebase-key.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_KEY)),
 });
 const https = require("https");
 
